79fe2cc4fd1a9bf3768c525150bac874a221e70e
NicoFox01 <nicolas.g1992@psi.uba.ar>
2026-02-01 20:30:24 -0300

    fixing database migration bugs, seeding and develop the testing module

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,5 +3,6 @@ from app.models.enums import PlanType, Roles, PaymentStatus, PaymentType
 from app.models.tenant import Tenants
 from app.models.user import Users
 from app.models.payments import Payments
+from app.models.notifications import Notification
 
 # Ahora Alembic puede ver todos los metadatos al importar 'app.models'
